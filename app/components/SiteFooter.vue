60eb84763087c19890b71653dd1bf41e8874a590
gunhun1027 <95231845+gunhun1027@users.noreply.github.com>
2025-05-02 12:12:06 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
+  github: "https://github.com/gunhun4018",
   home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  email: "mailto:admin@gunhun.dpdns.org",
 };
 </script>
 
